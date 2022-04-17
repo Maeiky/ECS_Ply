49cffa81e4b5ef322872b9042afff519865c1176
Maeiky <maeiky@gmail.com>
2022-04-17 06:47:59 -0400

    Fix

--- src/loadpng-sapp.c
+++ src/loadpng-sapp.c
@@ -282,7 +282,7 @@ sapp_desc sokol_main(int argc, char* argv[]) {
         .sample_count = 4,
 		.swap_interval = 1,
         .gl_force_gles2 = true,
-        .window_title = "Async PNG Loading (sokol-app)",
+        .window_title = "ECS Ply",
         .icon.sokol_default = true,
     };
 }
