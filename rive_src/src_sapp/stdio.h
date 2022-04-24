9e783558c999ecdf958b86dfcf4093167257bb87
maeiky <maeiky@gmail.com>
2022-04-23 21:33:50 -0400

    Add line number

--- rive_src/src_sapp/stdio.h
+++ rive_src/src_sapp/stdio.h
@@ -19,5 +19,13 @@ fn int _printf(const char *_format, ...);
 	#define _CR_ "\n"
 #endif
 
+#define STRINGIZE(x) STRINGIZE2(x)
+#define STRINGIZE2(x) #x
+#define LINE_STRING STRINGIZE(__LINE__)
+
+#ifdef D_Debug
+#define fprintf(FileStream, ...)  _printf(_CR_"E[Rive]" "(" __BASE_FILE__ ":" LINE_STRING  ")" "> " __VA_ARGS__);
+#else
 #define fprintf(FileStream, ...)  _printf(_CR_"E[Rive]> " __VA_ARGS__);
+#endif
 ///////////////////////////////////
